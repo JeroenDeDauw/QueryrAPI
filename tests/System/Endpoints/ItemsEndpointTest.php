934455f49fe9c43a20157ec0b6b5e091046d85dd
jeroendedauw <jeroendedauw@gmail.com>
2015-11-18 18:46:31 +0100

    Split item JSON building into dedicated methods in ItemsEndpointTest

--- tests/System/Endpoints/ItemsEndpointTest.php
+++ tests/System/Endpoints/ItemsEndpointTest.php
@@ -2,7 +2,6 @@
 
 namespace Queryr\WebApi\Tests\System\Endpoints;
 
-use Queryr\WebApi\UseCases\ListItems\ItemListingRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Wikibase\DataFixtures\Items\Berlin;
 use Wikibase\DataFixtures\Items\City;
@@ -38,6 +37,39 @@ class ItemsEndpointTest extends ApiTestCase {
 		$this->testEnvironment->insertItem( ( new City() )->newItem() );
 	}
 
+	private function getBerlinPreJson() {
+		return (object)[
+			'id'  => 'Q64',
+			'label'  => 'Berlin',
+			'updated_at'  => '0000',
+			'url'  => 'http://test.url/items/Q64',
+			'wikidata_url'  => 'https://www.wikidata.org/entity/Q64',
+			'wikipedia_url'  => 'https://www.wikidata.org/wiki/Special:GoToLinkedPage/enwiki/Q64',
+		];
+	}
+
+	private function getGermanyPreJson() {
+		return (object)[
+			'id'  => 'Q183',
+			'label'  => 'Germany',
+			'updated_at'  => '0000',
+			'url'  => 'http://test.url/items/Q183',
+			'wikidata_url'  => 'https://www.wikidata.org/entity/Q183',
+			'wikipedia_url'  => 'https://www.wikidata.org/wiki/Special:GoToLinkedPage/enwiki/Q183',
+		];
+	}
+
+	private function getCityPreJson() {
+		return (object)[
+			'id'  => 'Q515',
+			'label'  => 'city',
+			'updated_at'  => '0000',
+			'url'  => 'http://test.url/items/Q515',
+			'wikidata_url'  => 'https://www.wikidata.org/entity/Q515',
+			'wikipedia_url'  => 'https://www.wikidata.org/wiki/Special:GoToLinkedPage/enwiki/Q515',
+		];
+	}
+
 	public function testWhenThreeItems_theyAreAllReturned() {
 		$this->storeThreeItems();
 		$client = $this->createClient();
@@ -46,30 +78,9 @@ class ItemsEndpointTest extends ApiTestCase {
 
 		$this->assertJsonResponse(
 			[
-				(object)[
-					'id'  => 'Q64',
-					'label'  => 'Berlin',
-					'updated_at'  => '0000',
-					'url'  => 'http://test.url/items/Q64',
-					'wikidata_url'  => 'https://www.wikidata.org/entity/Q64',
-					'wikipedia_url'  => 'https://www.wikidata.org/wiki/Special:GoToLinkedPage/enwiki/Q64',
-				],
-				(object)[
-					'id'  => 'Q183',
-					'label'  => 'Germany',
-					'updated_at'  => '0000',
-					'url'  => 'http://test.url/items/Q183',
-					'wikidata_url'  => 'https://www.wikidata.org/entity/Q183',
-					'wikipedia_url'  => 'https://www.wikidata.org/wiki/Special:GoToLinkedPage/enwiki/Q183',
-				],
-				(object)[
-					'id'  => 'Q515',
-					'label'  => 'city',
-					'updated_at'  => '0000',
-					'url'  => 'http://test.url/items/Q515',
-					'wikidata_url'  => 'https://www.wikidata.org/entity/Q515',
-					'wikipedia_url'  => 'https://www.wikidata.org/wiki/Special:GoToLinkedPage/enwiki/Q515',
-				],
+				$this->getBerlinPreJson(),
+				$this->getGermanyPreJson(),
+				$this->getCityPreJson(),
 			],
 			$client->getResponse()
 		);
